8fcfb3aba249142db84eb0829dc8d8778c6339ea
mikinty <myou@andrew.cmu.edu>
2019-03-02 16:12:06 -0500

    device information and key mappings

---
+++ CONST.py
@@ -0,0 +1,20 @@
+# Device info
+VID = 0x12AB
+PID = 0x0004
+
+ARRAY_POS_ARROW  = 2
+ARRAY_POS_BUTTON = 3
+
+# Values from each arrow in ARRAY_POS_ARROW
+ARROW_UP    = 1
+ARROW_DOWN  = 2
+ARROW_LEFT  = 4
+ARROW_RIGHT = 8
+ARROW_START = 16
+ARROW_BACK  = 32
+
+# Value from each button in ARRAY_POS_BUTTON
+BUTTON_A = 16
+BUTTON_B = 32
+BUTTON_X = 64
+BUTTON_Y = 128
